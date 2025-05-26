2a06dab9a32977d0721ce7b8bf94f5be86ebbd1b
Guilherme Schons <gschonswow@gmail.com>
2025-05-26 17:06:36 -0300

    Remove trash

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "server.h"
 
 #define PORT 4221
@@ -8,18 +10,33 @@ int main(int argc, char *argv[]) {
     int port = PORT;
     char *file_server = "";
 
-    for (int i = 1; i < argc; i ++) {
+    for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--file-directory") == 0) {
-            i++;
-            file_server = argv[i];
-        } else {
-            int port = atoi(argv[i]);
-            if (port <= 0 || port > 65535) {
-                fprintf(stderr, "PORT out of valid range: %d\n", port);
+            if (i + 1 >= argc) {
+                fprintf(stderr, "Error: --file-directory requires a directory path\n");
                 exit(EXIT_FAILURE);
             }
+            i++;
+            file_server = argv[i];
+        }
+        else if (strcmp(argv[i], "--port") == 0) {
+            if (i + 1 >= argc) {
+                fprintf(stderr, "Error: --port requires a port number\n");
+                exit(EXIT_FAILURE);
+            }
+            i++;
+            int parsed_port = atoi(argv[i]);
+            if (parsed_port <= 0 || parsed_port > 65535) {
+                fprintf(stderr, "PORT out of valid range: %d\n", parsed_port);
+                exit(EXIT_FAILURE);
+            }
+            port = parsed_port;
         }
     }
+
+
+    printf("Starting HTTP Server on port %d...\n", port);
+    printf("Serving files from directory: %s\n", file_server);
 
     printf("Starting HTTP Server with Chunked Transfer Encoding...\n\n");
     run_server(port, file_server);
